798c447cfb895951e13a1f6662a186c31c64d2f1
nabilAparicio <orlando.nabil@gmail.com>
2022-03-25 05:07:08 -0500

    no comments

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,1 +1,27 @@
-import react from "../../node_modules/react/index";
+import react, { useState, useContext, createContext } from '../../node_modules/react/index';
+import jsCookie from '../../node_modules/js-cookie/index';
+import axios from '../../node_modules/axios/index';
+import endPoints from 'services/api/index';
+
+const AuthContext = createContext();
+
+export function ProviderAuth({ children }) {
+    const auth = useProviderAuth();
+    return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+}
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
+
+function useProviderAuth() {
+    const [user, setUser] = useState(null);
+
+    const signIn = async (email, password) => {
+        const { data: access_token } = await axios.post();
+    };
+    return {
+        user,
+        signIn,
+    };
+}
